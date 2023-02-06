323f86b4046644f6fdf9afdea4c3f24b32ccd89d
llcain <llcain2012@gmail.com>
2023-02-05 19:18:16 -0600

    added background images

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
-    document.querySelectorAll(".drum")[0].addEventListener("click", function () {
-        alert("I got clicked");
-
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        
         // What to do when click detected
+
+        var audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
     });
 }
